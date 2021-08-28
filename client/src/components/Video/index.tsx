98a0a55d9bdb3120e32603cd9fbdf68bda7d13f8
jp-knj <kenji.tomita1996@gmail.com>
2021-08-28 15:42:38 +0900

    :sparkles: Add Video Component

--- client/src/components/Video/index.tsx
+++ client/src/components/Video/index.tsx
@@ -1,6 +1,48 @@
+import {useState} from "react";
+
+const MicButton = () => {
+ const [isMuted, setIsMuted] = useState<boolean>(false)
+ const MicState = isMuted ? "OFF" : "ON"
+const handleMic = () => {
+  setIsMuted(!isMuted);
+}
+ return <div onClick={handleMic}>Mic:{MicState}</div>
+}
+const CameraButton = () => {
+    const [isCamera, setIsCamera] = useState<boolean>(false)
+    const CameraState = isCamera ? "OFF" : "ON"
+    const handleCamera = () => {
+        setIsCamera(!isCamera)
+    }
+ return <div onClick={handleCamera}>Camera:{CameraState}</div>
+}
+const LeaveButton = () => {
+    const handleLeave = () => {
+        const siteUrl = window.location.origin;
+        window.location.href = siteUrl;
+    }
+ return (<div>
+     <button onClick={handleLeave}>
+         Leave
+     </button>
+ </div>)
+}
+const SwitchToScreenSharingButton = () => {
+    const [ isScreenSharing, setIsScreenSharing] = useState(false)
+    const screenShareState = isScreenSharing ? "Not Share" : "Share!"
+    const handleScreenShare = () => {
+        setIsScreenSharing(!isScreenSharing)
+    }
+ return <div onClick={handleScreenShare}>{screenShareState}</div>
+}
 const Video = () => {
  return (
-     <div>Video</div>
+     <div>
+      <MicButton/>
+      <CameraButton/>
+      <LeaveButton />
+      <SwitchToScreenSharingButton />
+     </div>
  )
 }
 export  default  Video
